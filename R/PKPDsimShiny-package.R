47cbe473096ba465452bbe0bc5ce53851fce4ca1
ronkeizer <ronkeizer@gmail.com>
2015-12-07 11:20:59 -0800

    initial commit

---
+++ R/PKPDsimShiny-package.R
@@ -0,0 +1,9 @@
+#' PKPDsim package
+#'
+#' Simulate regimens for PKPD models defined by ODE systems
+#'
+#' @docType package
+#' @import PKPDsim dplyr ggplot2
+#' @name PKPDsim-package
+#' @author Ron Keizer \email{ronkeizer@@gmail.com}
+NULL
---
+++ tests/tests.R
@@ -0,0 +1,2 @@
+library(testit)
+library(PKPDsimShiny)
