2cb151220c9206fb6628c42a94a5891a630d300b
ronkeizer <ronkeizer@gmail.com>
2016-06-28 09:34:02 -0700

    clean up

--- tests/ex1.R
+++ tests/ex1.R
@@ -1,21 +1,22 @@
+Sys.setenv("R_TESTS" = "")
 # devtools::install_github("ronkeizer/PKPDsimShiny")
 library(PKPDsim)
 library(PKPDsimShiny)
 
 p <- list(CL = 1, V  = 10, KA = 0.5)
-pk1 <- new_ode_model("pk_1cmt_oral")
+pk1 <- new_ode_model(model = "pk_1cmt_oral")
 r1 <- new_regimen(amt = 100,
                   n = 5,
                   interval = 12)
 
-n <- 100
-omega <- c(0.1,
-           0.05, 0.1)
+# regular simulation into a dataset
+dat <- sim_ode (ode = "pk1",
+                par = p,
+                regimen = r1)
 
 # simulation in a Shiny app
-PKPDsimShiny::sim_ode_shiny (
-  ode = pk1,
-  par = p,
-  n_ind = n,
-  omega = omega,
-  regimen = r1)
+# sim_ode_shiny (ode = pk1,
+#                par = p,
+#                omega = c(0.1,
+#                          0.05, 0.1),
+#                regimen = r1)
